f8b799a25417b4cdcf6465e5bc70dbf375db9fd8
Macanthony Emmanuel <macanthonyemmanuel12@gmail.com>
2021-09-05 22:05:10 -0700

    implemented api views for drones app

--- drones/views.py
+++ drones/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from drones.models import DroneCategory
+from drones.models import Drone
+from drones.models import Pilot
+from drones.models import Competition
+from drones.serializers import DroneCategorySerializer
+from drones.serializers import DroneSerializer
+from drones.serializers import PilotSerializer
+from drones.serializers import PilotCompetitionSerializer
 
 # Create your views here.
+class DroneCategoryList(generics.ListCreateApiView): 
+    queryset = Drone.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-list'
+    
+class DroneCategoryDetail(generics.RetrieveUpdateDestroyAPIView): 
+    queryset = DroneCategory.objects.all()
+    serializer_class = DroneCategorySerializer
+    name = 'dronecategory-detail'
+    
+class DroneList(generics.ListCreateApiView): 
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = 'drone-list'
+    
+class DroneDetail(generics.RetrieveUpdateDestroyAPIView): 
+    queryset = Drone.objects.all()
+    serializer_class = DroneSerializer
+    name = 'drone-detail'
+    
+class PilotList(generics.ListCreateAPIView):
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-list'
+    
+class PilotDetail(generics.RetrieveUpdateDestroyAPIView): 
+    queryset = Pilot.objects.all()
+    serializer_class = PilotSerializer
+    name = 'pilot-detail'
+    
+class CompetitionList(generics.ListCreateAPIView): 
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = 'competition-list'
+    
+class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView): 
+    queryset = Competition.objects.all()
+    serializer_class = PilotCompetitionSerializer
+    name = 'competition-detail'
